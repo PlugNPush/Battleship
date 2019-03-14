9be090eeac4cfa0aca337fe86c028e2981674a9b
PlugNCS <michael.nass@free.fr>
2019-03-14 16:55:10 +0100

    Initial Commit

---
+++ Battleship/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Battleship
+//
+//  Created by PlugN on 14/03/2019.
+//  Copyright © 2019 Groupe MINASTE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
