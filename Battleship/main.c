c7aa14ab5f372b80ca1a9c66a3d41bc0e306513f
PlugNCS <michael.nass@free.fr>
2019-03-25 17:06:38 +0100

    ADV

--- Battleship/main.c
+++ Battleship/main.c
@@ -8,8 +8,98 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int playfield[10][10], playfield2[10][10];
+
+int placeBoats(int pf, int posx, int posy, int dir, int type){
+    if ((pf > 1 || pf < 0) || (dir > 1 || dir < 0) || (type > 50 || type < 20) || (posx > 10 || posx < 0) || (posy > 10 || posy < 0)){
+        // Unrealistic error
+        return 1;
+    }
+    
+    // dir = 0 -> horizontal | dir = 1 -> vertical
+    
+    if (pf == 0){
+        // Le joueur
+            int a;
+            int isPossible = 1;
+        
+            for (a = 0; a < type / 10 % 10; a++){
+                if (dir == 0){
+                    if (playfield[posx+a][posy] != 0) {
+                        isPossible = 0;
+                    }
+                } else if (dir == 1){
+                    if (playfield[posx][posy+a] != 0) {
+                        isPossible = 0;
+                    }
+                }
+        }
+        
+        if (isPossible == 0){
+            return 2;
+        }
+        
+        for (a = 0; a < type / 10 % 10; a++){
+            if (dir == 0){
+                playfield[posx+a][posy] = type;
+                } else if (dir == 1){
+                playfield[posx][posy+a] = type;
+            }
+        }
+        
+        // playfield[posx][posy] = type;
+    } else if (pf == 1){
+        // L'ordi
+        
+    }
+    
+    
+    
+    
+    
     return 0;
 }
+
+void game(){
+    
+    // Initiate the playfields
+    int i, s;
+    
+    // Playfield for the players #PS4
+    for (i = 0; i < 10; i++){
+        for (s = 0; s < 10; s++){
+            playfield[i][s] = 0;
+        }
+    }
+    
+    // Playfield for the computer
+    for (i = 0; i < 10; i++){
+        for (s = 0; s < 10; s++){
+            playfield2[i][s] = 0;
+        }
+    }
+    
+    // Place the boats
+    
+    
+    
+    return;
+}
+
+void mainmenu(){
+    
+    return;
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    mainmenu();
+    
+    return 0;
+    
+    
+}
+
+
+
